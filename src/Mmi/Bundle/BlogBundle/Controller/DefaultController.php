d266e2907de0a30ad4de2e68783699f301207913
JosePathe <johann-73@hotmail.fr>
2015-02-10 09:44:31 +0100

    ajout template

--- src/Mmi/Bundle/BlogBundle/Controller/DefaultController.php
+++ src/Mmi/Bundle/BlogBundle/Controller/DefaultController.php
@@ -6,6 +6,8 @@ namespace Mmi\Bundle\BlogBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+
 class DefaultController extends Controller
 {
     /**
@@ -14,6 +16,14 @@ class DefaultController extends Controller
     public function homepageAction()
     {
         return $this->get('templating')->renderResponse('MmiBlogBundle:Default:homepage.html.twig');
-
     }
+
+    /**
+     * @Route("/theme/{name}", name="blog_theme")
+     */
+    public function themeAction($name, Request $request)
+	{
+	    $request->getSession()->set('theme', $name);
+	    return $this->redirect($this->generateUrl('blog_homepage'));
+	}
 }
